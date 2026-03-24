ffb1c654b6ad1ba3b8bbcafafbb33fd40a9b7018
archanasivasrivarsha <charchana2007@gmail.com>
2026-03-24 14:40:32 +0530

    Add files via upload

---
+++ weird or not weird.py
@@ -0,0 +1,11 @@
+Python 3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> n = int(input())
+... if n % 2 != 0:
+...     print("weird")
+... else:
+...     if 2 <= n <=5:
+...         print("not weird")
+...     elif 6 <=n <=20:
+...         print("weird")
+...     else:
