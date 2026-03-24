8546a530e2ce3bc51611a0dc41cec4f927aabe2b
archanasivasrivarsha <charchana2007@gmail.com>
2026-03-24 11:53:07 +0530

    Add files via upload

---
+++ compound interest.py
@@ -0,0 +1,7 @@
+Python 3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> P=int(input())
+... R=int(input())
+... T=int(input())
+... Amount=P*(1+R/100)**T
+... CI=Amount-P
