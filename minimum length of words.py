365e0cd057d579c1394fd0286e3190fd793ef0ce
archanasivasrivarsha <charchana2007@gmail.com>
2026-03-26 14:01:33 +0530

    Add files via upload

---
+++ minimum length of words.py
@@ -0,0 +1,5 @@
+Python 3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> s = input()
+... words = s.split()
+... min_length = min(len(word) for word in words)
