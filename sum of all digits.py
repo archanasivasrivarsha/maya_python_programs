1601754065b947fdb6183a0811432943591ff4c9
archanasivasrivarsha <charchana2007@gmail.com>
2026-03-26 13:47:13 +0530

    Add files via upload

---
+++ sum of all digits.py
@@ -0,0 +1,9 @@
+Python 3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> n = int(input())
+... n = abs(n)
+... sum_digits = 0
+... while n > 0:
+...     digit = n % 10
+...     sum_digits += digit
+...     n = n // 10
