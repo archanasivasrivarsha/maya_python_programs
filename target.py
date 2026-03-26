2937c3bf17b0195e3175bfff7e1af71fba5430cd
archanasivasrivarsha <charchana2007@gmail.com>
2026-03-26 13:14:11 +0530

    Add files via upload

---
+++ target.py
@@ -0,0 +1,6 @@
+Python 3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> P1, P2, P3, P4 = map(int, input().split())
+... if P1 >= 10 and P2 >= 10 and P3 >= 10 and P4 >= 10:
+...     print("YES")
+... else:
