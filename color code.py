f3cc9d91613358e5259626e65fb9ed73c89a4882
archanasivasrivarsha <charchana2007@gmail.com>
2026-03-26 13:32:28 +0530

    Add files via upload

---
+++ color code.py
@@ -0,0 +1,18 @@
+Python 3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> code = input().strip().upper()
+... if code == 'V':
+...     print("Violet")
+... elif code == 'I':
+...     print("Indigo")
+... elif code == 'B':
+...     print("Blue")
+... elif code == 'G':
+...     print("Green")
+... elif code == 'Y':
+...     print("Yellow")
+... elif code == 'O':
+...     print("Orange")
+... elif code == 'R':
+...     print("Red")
+... else:
