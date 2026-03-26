390b9d578b9b356503d175f7cd9204405b2cb4a1
archanasivasrivarsha <charchana2007@gmail.com>
2026-03-26 14:29:09 +0530

    Add files via upload

---
+++ divide array into equal parts.py
@@ -0,0 +1,20 @@
+Python 3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> n = int(input())
+... nums = list(map(int, input().split()))
+... 
+... freq = {}
+... 
+... # Count frequencies
+... for num in nums:
+...     if num in freq:
+...         freq[num] += 1
+...     else:
+...         freq[num] = 1
+... 
+... # Check if all counts are even
+... for value in freq.values():
+...     if value % 2 != 0:
+...         print("false")
+...         break
+... else:
